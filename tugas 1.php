8aa80b81aa5cdfc4c94dd7c4308b3d14e0e9bf94
RamadhanS123 <111478906+RamadhanS123@users.noreply.github.com>
2023-01-20 06:43:23 +0700

    Create tugas 1.php

---
+++ tugas 1.php
@@ -0,0 +1,18 @@
+<?php
+echo "Plaintext : Ramadhan Saputra Alpani<br>";
+echo "Chipertext:";
+echo chr(ord('W')+15);
+echo chr(ord('I')+15);
+echo chr(ord('S')+15);
+echo chr(ord('N')-11);
+echo chr(ord('U')+15);
+echo chr(ord(' ')+15);
+echo chr(ord('P')-11);
+echo chr(ord('A')+15);
+echo chr(ord('R')+15);
+echo chr(ord('T')+15);
+echo chr(ord('A')+15);
+echo chr(ord('M')-11);
+echo chr(ord('A')+15);
+echo "<br>Kriptografi Kelas 7.2"
+?>
